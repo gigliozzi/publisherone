c2ce2de22a8d74595c480a79a09400c805117009
William Gigliozzi <gigliozzi@gmail.com>
2021-05-18 00:34:37 -0300

    Project Created

---
+++ backend/src/main/java/com/softcode/cardpublisher/CardpublisherApplication.java
@@ -0,0 +1,13 @@
+package com.softcode.cardpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardpublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardpublisherApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/softcode/cardpublisher/CardpublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softcode.cardpublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardpublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
